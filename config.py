537b2a669df7cd18e4f10993b464de8644132730
ingelm <henrymoraleshl@gmail.com>
2025-07-03 00:50:50 -0400

    inicio/DATABASEURL

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '$2a$12$EuLRBS0CW4zeJjGKK6zEPeC/cwuCG71/qWfV42fnlZSNMyzs/YD5u'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://globalstweb:globalst062022@localhost/globalstweb_sag'  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
     DEBUG = True
