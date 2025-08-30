c82f21b2e215b578b12f9c1252a1366e83e09c36
ingelm <henrymoraleshl@gmail.com>
2025-08-29 20:53:07 -0400

    chg/passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,1 @@
-# passenger_wsgi.py
-
-import sys
-import codecs
-import os
-
-# Aseguramos que stdout y stderr usen UTF-8
-sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
-sys.stderr = codecs.getwriter('utf-8')(sys.stderr.detach())
-
-# Agregamos el directorio actual al path
-sys.path.append(os.getcwd())
-
-# Importamos la app Flask desde tu archivo principal
 from run import app as application
