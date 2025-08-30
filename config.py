a609018c1853f1d24239d34b9a3da8bd8c58d919
ingelm <henrymoraleshl@gmail.com>
2025-08-29 21:57:15 -0400

    chg/secret_key

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY', '$2a$12$jtoF3Txp11ljagYvW1oorO5U9mPUhW/rl7R6j.fS4jkAg.So.TCpG')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://globalstweb:ib2;rQ55Dp:P0S@globalst.com.ve/globalstweb_sag')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
