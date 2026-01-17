f4eb2dbb89b0c51c678e3a5fbf87662ea357c4f5
ingelm <henrymoraleshl@gmail.com>
2026-01-17 09:15:54 -0400

    fix/caracter especial

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,11 @@ import sys
 import codecs
 import os
 
+
+# Forzar el uso de UTF-8 para el sistema de archivos y el entorno
+os.environ["LANG"] = "en_US.UTF-8"
+os.environ["LC_ALL"] = "en_US.UTF-8"
+
 # Aseguramos que stdout y stderr usen UTF-8
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
 sys.stderr = codecs.getwriter('utf-8')(sys.stderr.detach())
