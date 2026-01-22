66fa939edfda6230a93675720cd633d57080cd6b
ingelm <henrymoraleshl@gmail.com>
2026-01-21 22:55:37 -0400

    control2

--- app/login/routes.py
+++ app/login/routes.py
@@ -52,10 +52,14 @@ def login():
             empleadosModel.query.all()
             # flash(f'Conectado correctamente a la Base de datos: \n {Config.SQLALCHEMY_DATABASE_URI}', 'success' )
             flash(f'Conectado correctamente a la Base de datos {Config.CONECTADO_A}', 'success' )
+            if Config.CONECTADO_A == 'PRODUCCIÓN':
+                mostrar = ''
+            else:
+                mostrar = 'd-none'
         except Exception as e:
             flash(f'Error en la conexión con la Base de Datos, recargue la página, si el problema persiste contacte al administrador.', 'danger')
             #    flash(f'Error en la conexión con la Base de Datos, recargue la página, si el problema persiste contacte al administrador', 'danger')
-        return render_template('login.html', form=form, year=datetime.now().year)
+        return render_template('login.html', form=form, year=datetime.now().year, mostrar=mostrar)
 
 @login_bp.route('/logout')
 def logout():
